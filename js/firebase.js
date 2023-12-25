c432addda4cf229a1b0f84dd72f2ac7253502497
krunal-08 <154742529+krunal-08@users.noreply.github.com>
2023-12-25 14:01:27 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBoQ5ntVNxXENwRkAVEA-qJf4iRKoE76lk",
-  authDomain: "tutorial-ea726.firebaseapp.com",
-  projectId: "tutorial-ea726",
-  storageBucket: "tutorial-ea726.appspot.com",
-  messagingSenderId: "1048781684417",
-  appId: "1:1048781684417:web:8b78ace85c33ab94b29e40",
-  measurementId: "G-LNF7ZN2T25"
+  apiKey: "AIzaSyCqS8ckp2BAaf_TlD0bE834_Xa8ObtyY8w",
+  authDomain: "selfie-media.firebaseapp.com",
+  projectId: "selfie-media",
+  storageBucket: "selfie-media.appspot.com",
+  messagingSenderId: "95908536556",
+  appId: "1:95908536556:web:bee7f01dd12ef44e78c5c9",
+  measurementId: "G-LNDBP0RDN5"
   };
 
   // Initialize Firebase
